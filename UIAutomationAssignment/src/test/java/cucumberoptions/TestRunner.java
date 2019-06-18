af51513ca30a029c7a48b41fcd9c8f21c97e979a
nobelharvards <nobelharvards@users.noreply.github.com>
2019-06-18 13:08:46 +1000

    Add files via upload

--- UIAutomationAssignment/src/test/java/cucumberoptions/TestRunner.java
+++ UIAutomationAssignment/src/test/java/cucumberoptions/TestRunner.java
@@ -1,12 +1,8 @@
 package cucumberoptions;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/featurefiles", glue = "stepdefinitions")
 public class TestRunner extends AbstractTestNGCucumberTests
 {
